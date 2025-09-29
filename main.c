0d8654cbee91543907a73dc92cf771623d1bc9d6
birbhimself <birbhimself@github.com>
2025-09-29 20:40:33 +0000

    fixed prompt issues, fork() does not work anymore

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-#include <bits/types/siginfo_t.h>
 #include <limits.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,32 +6,82 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
+#include <readline/history.h>
+#include <readline/readline.h>
 
 #define MAX_CHARS 4096
 #define MAX_ARGS 128
 
+#define clear() printf("\033[H\033[J")
+
 char buffer[MAX_CHARS];
 char prompt[128];
 
-void getInput(char* buf)
+int getInput(char* buf)
 {
-  if(fgets(buf, MAX_CHARS, stdin) != NULL)
+  char* str;
+  // if(fgets(buf, MAX_CHARS, stdin) != NULL)
+  // {
+  //   buf[strcspn(buf, "\n")] = '\0';    
+  //   return; 
+  // }
+  str = readline(prompt);
+  if(str != NULL)
   {
-    buf[strcspn(buf, "\n")] = '\0';    
-    return; 
+    add_history(str);
+    strncpy(buf, str, strlen(buf));
+    return 0;
   }
+  else
+  {
+    return -1;
   }
-
-void drawPrompt(char* username, char* hostname)
-{
-  snprintf(prompt,128,"[%s@%s@]-->",hostname,username);
-  printf("%s",prompt);
 }
 
-int parseinput(char* buf, char *hostname, char *username)
+void createPrompt(char* username, char* hostname)
+{
+  snprintf(prompt,128,"[%s@%s@]-->",hostname,username);
+}
+
+// int parseinput(char* buf, char *hostname, char *username)
+// {
+//   int command_counter = 0;
+//   int argc = 0;
+//   char **args = (char**)calloc(MAX_ARGS, sizeof(*args));
+//   char *token = strtok(buf, " ");
+//   for(int i = 0;token != NULL;i++)
+//   {
+//     args[i] = token;
+//     token = strtok(NULL, " ");
+//     argc++;
+//   }
+//   args[argc] = NULL;
+//   int status;
+//   pid_t child = fork();
+//   if(child < 0)
+//   {
+//     return -1;
+//   }
+//   else if(child == 0)
+//   {
+//     command_counter++;
+//     execvp(args[0], args);
+//     return -1;
+//   }
+//   while(!waitpid(child,&status,WEXITSTATUS(status)));
+//   drawPrompt(username, hostname);
+//   return 0;
+// }
+
+int tokenize(char* buf, char **args)
 {
   int argc = 0;
-  char **args = (char**)calloc(MAX_ARGS, sizeof(*args));
+  args = (char**)calloc(MAX_ARGS, sizeof(*args));
+  if(args == NULL)
+  {
+    printf("FATAL ERROR: Malloc failed");
+    return 1;
+  }
   char *token = strtok(buf, " ");
   for(int i = 0;token != NULL;i++)
   {
@@ -42,7 +90,13 @@ int parseinput(char* buf, char *hostname, char *username)
     argc++;
   }
   args[argc] = NULL;
+  return 0;
+}
+
+int runCommand(char **args, int argc)
+{
   int status;
+  int command_counter = 0;
   pid_t child = fork();
   if(child < 0)
   {
@@ -50,11 +104,9 @@ int parseinput(char* buf, char *hostname, char *username)
   }
   else if(child == 0)
   {
+    command_counter++;
     execvp(args[0], args);
-    return -1;
   }
-  while(!waitpid(child,&status,WEXITSTATUS(status)));
-  drawPrompt(username, hostname);
   return 0;
 }
 
@@ -66,17 +118,20 @@ __sighandler_t handlesigint()
 
 int main()
 {
+  char **args;
+  int argc;
   char hostname[HOST_NAME_MAX + 1];
   char *username = getenv("USER");
   if(gethostname(hostname, HOST_NAME_MAX) == -1)
   {
     strncpy(hostname,"fallback-da-silva", strlen(hostname));
   }
-  drawPrompt(hostname, username);
+  createPrompt(hostname, username);
   do
   {
     getInput(buffer);
-    parseinput(buffer,hostname,username);
+    tokenize(buffer, args);
+    runCommand(args, argc);
   }while(1);
   return EXIT_SUCCESS;
 }
